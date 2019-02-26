fe00d1d650e28b00acf34d4e563de156a668e15b
Henrique Rodrigues <henryqrm@gmail.com>
2019-02-26 17:06:45 -0300

    fix custom airootfs

--- src/archiso/airootfs/root/customize_airootfs.sh
+++ src/archiso/airootfs/root/customize_airootfs.sh
@@ -27,62 +27,61 @@ function configRootUserFunc() {
 function editOrCreateConfigFilesFunc () {
 
     # Locale
-    # echo "LANG=en_US.UTF-8" > /etc/locale.conf
+    echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
 
     sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
     sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
     sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 }
 
-# function createLiveUserFunc () {
-# 	# add liveuser
-# 	useradd -m liveuser -u 500 -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash
-#   chown -R liveuser:users /home/liveuser
+function createLiveUserFunc () {
+	# add liveuser
+    useradd -m liveuser -u 500 -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash
+    chown -R liveuser:users /home/liveuser
 
-#   #enable autologin
-#   groupadd -r autologin
-#   gpasswd -a liveuser autologin
+    #enable autologin
+    groupadd -r autologin
+    gpasswd -a liveuser autologin
 
-# 	groupadd -r nopasswdlogin
-# 	gpasswd -a liveuser nopasswdlogin
-# }
+    groupadd -r nopasswdlogin
+    gpasswd -a liveuser nopasswdlogin
+}
 
-# function setDefaultsFunc() {
-#     export _EDITOR=nano
-#     echo "EDITOR=${_EDITOR}" >> /etc/environment
-#     #echo "EDITOR=${_EDITOR}" >> /etc/skel/.bashrc
-#     echo "EDITOR=${_EDITOR}" >> /etc/profile
-# }
+function setDefaultsFunc() {
+    export _EDITOR=nano
+    echo "EDITOR=${_EDITOR}" >> /etc/environment
+    #echo "EDITOR=${_EDITOR}" >> /etc/skel/.bashrc
+    echo "EDITOR=${_EDITOR}" >> /etc/profile
+}
 
 function enableServicesFunc() {
-    # systemctl enable pacman-init.service choose-mirror.service
     systemctl set-default multi-user.target
-# 	systemctl enable lightdm.service
-# 	systemctl set-default graphical.target
-# 	systemctl enable NetworkManager.service
-# 	systemctl enable virtual-machine-check.service
-# 	systemctl enable reflector.service
-# 	systemctl enable reflector.timer
-#   systemctl enable org.cups.cupsd.service
-#   systemctl enable bluetooth.service
-#   systemctl enable ntpd.service
-  #systemctl enable smb.service
-  #systemctl enable nmb.service
-  #systemctl enable winbind.service
-#   systemctl enable avahi-daemon.service
-#   systemctl enable avahi-daemon.socket
-  #systemctl enable tlp.service
-  #systemctl enable tlp-sleep.service
-  #systemctl enable vnstat.service
+	systemctl enable lightdm.service
+	systemctl set-default graphical.target
+	systemctl enable NetworkManager.service
+	systemctl enable virtual-machine-check.service
+	systemctl enable reflector.service
+	systemctl enable reflector.timer
+    systemctl enable org.cups.cupsd.service
+    systemctl enable bluetooth.service
+    systemctl enable ntpd.service
+    #systemctl enable smb.service
+    #systemctl enable nmb.service
+    #systemctl enable winbind.service
+    systemctl enable avahi-daemon.service
+    systemctl enable avahi-daemon.socket
+#   systemctl enable tlp.service
+#   systemctl enable tlp-sleep.service
+#   systemctl enable vnstat.service
 }
 
 
-# function fixWifiFunc() {
-#     #https://wiki.archlinux.org/index.php/NetworkManager#Configuring_MAC_Address_Randomization
-#     su -c 'echo "" >> /etc/NetworkManager/NetworkManager.conf'
-#     su -c 'echo "[device]" >> /etc/NetworkManager/NetworkManager.conf'
-#     su -c 'echo "wifi.scan-rand-mac-address=no" >> /etc/NetworkManager/NetworkManager.conf'
-# }
+function fixWifiFunc() {
+    #https://wiki.archlinux.org/index.php/NetworkManager#Configuring_MAC_Address_Randomization
+    su -c 'echo "" >> /etc/NetworkManager/NetworkManager.conf'
+    su -c 'echo "[device]" >> /etc/NetworkManager/NetworkManager.conf'
+    su -c 'echo "wifi.scan-rand-mac-address=no" >> /etc/NetworkManager/NetworkManager.conf'
+}
 
 
 function fixHibernateFunc() {
@@ -100,11 +99,11 @@ function initkeysFunc() {
     #sudo pacman-key --refresh-keys
 }
 
-# function getNewMirrorCleanAndUpgrade() {
-#     reflector --protocol https --latest 50 --number 20 --sort rate --save /etc/pacman.d/mirrorlist
-#     pacman -Sc --noconfirm
-#     pacman -Syyu --noconfirm
-# }
+function getNewMirrorCleanAndUpgrade() {
+    reflector --protocol https --latest 50 --number 20 --sort rate --save /etc/pacman.d/mirrorlist
+    pacman -Sc --noconfirm
+    pacman -Syyu --noconfirm
+}
 
 umaskFunc
 echo "#####   Function umaskFunc done    #####"
@@ -116,17 +115,17 @@ editOrCreateConfigFilesFunc
 echo "#####   Function editOrCreateConfigFilesFunc done    #####"
 configRootUserFunc
 echo "#####   Function configRootUserFunc done    #####"
-# createLiveUserFunc
-# echo "#####   Function createLiveUserFunc done    #####"
-# setDefaultsFunc
-# echo "#####   Function setDefaultsFunc done    #####"
+createLiveUserFunc
+echo "#####   Function createLiveUserFunc done    #####"
+setDefaultsFunc
+echo "#####   Function setDefaultsFunc done    #####"
 enableServicesFunc
 echo "#####   Function enableServicesFunc done    #####"
-# fixWifiFunc
-# echo "#####   Function fixWifiFunc done    #####"
+fixWifiFunc
+echo "#####   Function fixWifiFunc done    #####"
 fixHibernateFunc
 echo "#####   Function fixHibernateFunc done    #####"
-# initkeysFunc
-# echo "#####   Function  initkeysFunc done    #####"
-# getNewMirrorCleanAndUpgrade
-# echo "#####   Function getNewMirrorCleanAndUpgrade done    #####"
+initkeysFunc
+echo "#####   Function  initkeysFunc done    #####"
+getNewMirrorCleanAndUpgrade
+echo "#####   Function getNewMirrorCleanAndUpgrade done    #####"
