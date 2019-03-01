d22dd065f929728ec8c82680c78685a1d7f8b8f4
Henrique Rodrigues <henryqrm@gmail.com>
2019-02-28 22:37:01 -0300

    fix create iso

--- create_iso.sh
+++ create_iso.sh
@@ -9,71 +9,9 @@ sudo mkdir ./.tmp/
 sudo cp -rf ./src/* ./.tmp/
 
 echo "Checking if archiso is installed"
-
-package="archiso"
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-		echo "################################################################"
-		echo "################## "$package" is already installed"
-		echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi yay &> /dev/null; then
-
-		echo "################################################################"
-		echo "######### Installing with yay"
-		echo "################################################################"
-		yay -S --noconfirm $package
-
-	elif pacman -Qi trizen &> /dev/null; then
-
-		echo "################################################################"
-		echo "######### Installing with trizen"
-		echo "################################################################"
-		trizen -S --noconfirm --needed --noedit $package
-
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "################################################################"
-		echo "######### Installing with yaourt"
-		echo "################################################################"
-		yaourt -S --noconfirm $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-
-		echo "################################################################"
-		echo "######### Installing with pacaur"
-		echo "################################################################"
-		pacaur -S --noconfirm --noedit  $package
-
-	elif pacman -Qi packer &> /dev/null; then
-
-		echo "################################################################"
-		echo "######### Installing with packer"
-		echo "################################################################"
-		packer -S --noconfirm --noedit  $package
-
-	fi
-
-	# Just checking if installation was successful
-	if pacman -Qi $package &> /dev/null; then
-
-		echo "################################################################"
-		echo "#########  "$package" has been installed"
-		echo "################################################################"
-
-	else
-
-		echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-		echo "!!!!!!!!!  "$package" has NOT been installed"
-		echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-	fi
-
+if ! paccheck archiso &> /dev/null; then
+	echo "Installing archiso"
+	yay -S --noconfirm archiso
 fi
 
 echo "Copying files and folder to ./build as root"
@@ -91,8 +29,9 @@ echo "Building the iso"
 sudo ./build.sh -v
 
 echo "Moving the iso to ./iso"
-[ -d  ~/iso ] || mkdir ~/iso
-sudo cp ./.tmp/out/* ~/iso
+[ -d  ../iso ] || mkdir ../iso
+sudo cp ./out/* ../iso
 
 echo "Deleting the .tmp folder"
+cd ..
 [ -d ./.tmp/ ] && sudo rm -rf ./.tmp/
