8844460af6bca5b0fee27a6e5daddbcfd4e4cb0e
Henrique Rodrigues <henryqrm@gmail.com>
2019-03-06 11:35:36 -0300

    fix names and remove lines

--- src/airootfs/etc/skel/.config/polybar/launch.sh
+++ src/airootfs/etc/skel/.config/polybar/launch.sh
@@ -2,8 +2,6 @@
 
 # More info : https://github.com/jaagr/polybar/wiki
 
-# Install the following applications for polybar and icons in polybar if you are on ArcoLinuxD
-# awesome-terminal-fonts
 # Tip : There are other interesting fonts that provide icons like nerd-fonts-complete
 
 # Terminate already running bar instances
